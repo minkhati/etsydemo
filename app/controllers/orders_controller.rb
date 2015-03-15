282fd61872d6469eaf4e21190edde8245abc2dcb
Min Khati <minkhati@cdcsit.edu.np>
2015-03-15 18:35:58 +0000

    Transfer funds to seller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -37,7 +37,18 @@ class OrdersController < ApplicationController
     rescue  Stripe::CardError =>e
       flash[:danger] = e.message
     end
-  
+    
+    begin
+      transfer = Stripe::Transfer.create(
+        :amount => (@listing.price * 95).floor,
+        :currency => "usd",
+        :recipient => @seller.recipient
+        )
+      flash[:notice] = "Thanks for ordering!"     
+    rescue  Stripe::CardError =>e
+      flash[:danger] = e.message
+    end
+          
     respond_to do |format|
       if @order.save
         format.html { redirect_to root_url }
